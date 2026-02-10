6bf9b40f282e95b2db9e78bdcb025ab44fdf5d30
Abhi-7-github <abhikollepara333@gmail.com>
2026-02-10 13:57:38 +0530

    done

--- server/app.js
+++ server/app.js
@@ -6,7 +6,10 @@ const questionRoutes = require('./routes/questions');
 const submissionRoutes = require('./routes/submissions');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || '*',
+    credentials: true
+}));
 app.use(express.json());
 app.use('/api/questions', questionRoutes);
 app.use('/api', submissionRoutes);
