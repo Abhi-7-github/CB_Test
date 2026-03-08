f3bbea19610cf72f716bd4a570bde0146a67a747
Abhi-7-github <abhikollepara333@gmail.com>
2026-03-08 12:24:24 +0530

    new data

--- client/src/pages/StudentQuestions.jsx
+++ client/src/pages/StudentQuestions.jsx
@@ -16,7 +16,7 @@ function StudentQuestions() {
   const [submitStatus, setSubmitStatus] = useState({ type: 'idle', message: '' })
   const [answers, setAnswers] = useState({})
   const [markedForReview, setMarkedForReview] = useState({})
-  const examDurationSeconds = 60 * 60
+  const examDurationSeconds = 20 * 60
   const [remainingSeconds, setRemainingSeconds] = useState(examDurationSeconds)
   const autoSubmitTriggeredRef = useRef(false)
   const screenPreviewRef = useRef(null)
